c1abc77a12d5acdaeb01063257e64eb985559336
dancefactory <youngkinam@yahoo.com>
2023-03-06 17:12:20 +0900

    ContextConfiguration error

--- src/main/java/com/fastcampus/ch3/DBConnectionTest2.java
+++ src/main/java/com/fastcampus/ch3/DBConnectionTest2.java
@@ -1,15 +1,24 @@
 package com.fastcampus.ch3;
 
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:/src/main/webapp/WEB-INF/**/root-context.xml"})
 public class DBConnectionTest2 {
-   // @Test
+    @Autowired
+    static
+    DataSource ds;
+   @Test
     //public void jdbcConnectionTest() throws Exception {
    public static void main(String[] args) throws Exception   {
 
@@ -28,8 +37,8 @@ public class DBConnectionTest2 {
 //        ds.setUsername(DB_USER);
 //        ds.setPassword(DB_PASSWORD);
 
-        ApplicationContext ac = new GenericXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/**/root-context.xml");
-        DataSource ds = ac.getBean(DataSource.class);
+//        ApplicationContext ac = new GenericXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/**/root-context.xml");
+//        DataSource ds = ac.getBean(DataSource.class);
 
         Connection conn = ds.getConnection(); // 데이터베이스의 연결을 얻는다.
 
