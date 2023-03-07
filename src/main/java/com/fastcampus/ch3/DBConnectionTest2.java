4b6053bcef3cd9724289dea29893fe1b4b55d60c
dancefactory <youngkinam@yahoo.com>
2023-03-07 18:39:59 +0900

    tomcat

--- src/main/java/com/fastcampus/ch3/DBConnectionTest2.java
+++ src/main/java/com/fastcampus/ch3/DBConnectionTest2.java
@@ -1,44 +1,17 @@
 package com.fastcampus.ch3;
 
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"file:/src/main/webapp/WEB-INF/**/root-context.xml"})
 public class DBConnectionTest2 {
-    @Autowired
-    static
-    DataSource ds;
-   @Test
-    //public void jdbcConnectionTest() throws Exception {
+
    public static void main(String[] args) throws Exception   {
 
-
-//        // 스키마의 이름(springbasic)이 다른 경우 알맞게 변경
-//        String DB_URL = "jdbc:mysql://imcmaster.iptime.org:3306/springbasic?useUnicode=true&characterEncoding=utf8";
-//
-//        // DB의 userid와 pwd를 알맞게 변경
-//        String DB_USER = "root";
-//        String DB_PASSWORD = "ykn9080";
-//        String DB_DRIVER = "com.mysql.jdbc.Driver";
-//
-//        DriverManagerDataSource ds = new DriverManagerDataSource();
-//        ds.setDriverClassName(DB_DRIVER);
-//        ds.setUrl(DB_URL);
-//        ds.setUsername(DB_USER);
-//        ds.setPassword(DB_PASSWORD);
-
-//        ApplicationContext ac = new GenericXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/**/root-context.xml");
-//        DataSource ds = ac.getBean(DataSource.class);
+        ApplicationContext ac = new GenericXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/**/root-context.xml");
+        DataSource ds = ac.getBean(DataSource.class);
 
         Connection conn = ds.getConnection(); // 데이터베이스의 연결을 얻는다.
 
@@ -46,19 +19,3 @@ public class DBConnectionTest2 {
         //assertTrue(conn!=null);
     }
 }
-
-/* [root-context.xml]
-<?xml version="1.0" encoding="UTF-8"?>
-<beans xmlns="http://www.springframework.org/schema/beans"
-	   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-	   xsi:schemaLocation="http://www.springframework.org/schema/beans https://www.springframework.org/schema/beans/spring-beans.xsd">
-
-	<!-- Root Context: defines shared resources visible to all other web components -->
-	<bean id="dataSource" class="org.springframework.jdbc.datasource.DriverManagerDataSource">
-		<property name="driverClassName" value="com.mysql.jdbc.Driver"></property>
-		<property name="url" value="jdbc:mysql://localhost:3306/springbasic?useUnicode=true&amp;characterEncoding=utf8"></property>
-		<property name="username" value="asdf"></property>
-		<property name="password" value="1234"></property>
-	</bean>
-</beans>
-*/
--- src/main/java/com/fastcampus/ch3/UserDaoImplTest.java
+++ src/main/java/com/fastcampus/ch3/UserDaoImplTest.java
@@ -1,27 +1,35 @@
 package com.fastcampus.ch3;
 
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.*;
+import org.junit.runner.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.test.context.*;
+import org.springframework.test.context.junit4.*;
+
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
-class UserDaoImplTest {
+@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class UserDaoImplTest {
     @Autowired
     UserDao userDao;
     @Test
-    void selectUser() {
+    public void selectUser() {
     }
 
     @Test
-    void updateUser() {
+    public void updateUser() {
+        User user=new User("asdf21", "1234", "abc", "ykn@g.com", new Date(), "fb", new Date());
+        int rowCnt=userDao.insertUser(user);
+        assertEquals(rowCnt, 1);
     }
 
     @Test
-    void insertUser() {
+    public void insertUser() {
     }
 }
